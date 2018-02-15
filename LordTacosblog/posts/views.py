3bc5053a56759d52c0b5ae2d9e44a209c5865aef
Gustavo Antonio Lastra Colorado <gustavo.lastrac@gmail.com>
2018-02-15 11:24:06 +0100

    Bug on URL pattern
    ?: (urls.E004) Your URL pattern [<RegexURLResolver <RegexURLPattern list> (admin:admin) ^admin/>, <RegexURLPattern None ^$>, <RegexURLPattern None ^posts/(?P<post_id>[0-9]+)/$>, <RegexURLPattern None ^pics\/(?P<path>.*)$>] is invalid. Ensure that urlpatterns is a list of url() instances.

--- LordTacosblog/posts/views.py
+++ LordTacosblog/posts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Post
 # Create your views here.
 def home(request):
-
     posts =  Post.objects.order_by('pub_date')
     return render(request, 'posts/home.html', {'posts': posts})
+
+def post_details(request, post_id):
+    return render(request, 'posts/post_details.html', {'post_id': post_id})
