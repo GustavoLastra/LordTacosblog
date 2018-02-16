27fea579e975421707956f49dc8c21da62ecf66b
Gustavo Antonio Lastra Colorado <gustavo.lastrac@gmail.com>
2018-02-16 16:30:02 +0100

    add post_details ui

--- LordTacosblog/LordTacosblog/urls.py
+++ LordTacosblog/LordTacosblog/urls.py
@@ -7,7 +7,7 @@ from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home ),
-    url(r'^posts/(?P<post_id>[0-9]+)/$', views.post_details ),
+    url(r'^$', views.home),
+    url(r'^posts/(?P<post_id>[0-9]+)/$', views.post_details, name="post_details")
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
