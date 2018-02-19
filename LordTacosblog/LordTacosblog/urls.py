d9c81b748d85c80f8cc84a02fda365c4362e3877
Gustavo Antonio Lastra Colorado <gustavo.lastrac@gmail.com>
2018-02-19 13:16:24 +0100

    add about app

--- LordTacosblog/LordTacosblog/urls.py
+++ LordTacosblog/LordTacosblog/urls.py
@@ -1,13 +1,15 @@
 
 from django.conf.urls import url
 from django.contrib import admin
-from posts import views
+import posts.views
+import about.views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.home),
-    url(r'^posts/(?P<post_id>[0-9]+)/$', views.post_details, name="post_details")
+    url(r'^$', posts.views.home, name='home'),
+    url(r'^posts/(?P<post_id>[0-9]+)/$', posts.views.post_details, name="post_details"),
+    url(r'^about/', about.views.about, name='about'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
